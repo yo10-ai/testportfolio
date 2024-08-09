66c9e779937f557c30f7ce26fa43634a9b4fcd10
yogesh <71713475+yo10-ai@users.noreply.github.com>
2024-08-09 13:02:46 +0000

    edited dev and test.js to check changes for code and fontsize

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
-console.log("helloworld")
+//almost never use var as a keyword to declare a variable
+const accountId = 936790;
+let accountName = 'yogesh';
+var accountEmail = 'yg78534@gmail.com';
+accountCity = 'Pune';
+
+console.log([accountId,accountName,accountEmail,accountCity]);

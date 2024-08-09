609cd58b540c4bef51481e958d080d6932b66246
yogesh <71713475+yo10-ai@users.noreply.github.com>
2024-08-09 13:08:07 +0000

    changes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,7 +1,7 @@
-//almost never use var as a keyword to declare a variable
+//almost never use var as a keyword to declare a variable due to issue in blockscope and functional scope
 const accountId = 936790;
 let accountName = 'yogesh';
 var accountEmail = 'yg78534@gmail.com';
-accountCity = 'Pune';
-
-console.log([accountId,accountName,accountEmail,accountCity]);
+accountCity = 'Pune';//can declare a variable without using any keyword
+let accountState;
+console.table([accountId,accountName,accountEmail,accountCity, accountState]);

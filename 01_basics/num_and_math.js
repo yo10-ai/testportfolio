35e1c14e33e36e9f4499b09545ffc90ca3437c79
yogesh <71713475+yo10-ai@users.noreply.github.com>
2024-10-07 05:15:46 +0000

    octseventutorial

---
+++ 01_basics/num_and_math.js
@@ -0,0 +1,8 @@
+// const number = 400;
+// console.log(number);
+// const example = new Number(450);
+// console.log(example.toString());
+// console.log(typeof(example.toString()));
+const number =10000000;
+// console.log(number.toLocaleString('en-IN'));
+console.log(Math.Max(1,2,3));
---
+++ 01_basics/twentysevensep.js
@@ -0,0 +1,8 @@
+let id=Symbol('123');
+let anotherid=Symbol('123');
+//console.log(id===anotherid);
+//const testfunction = function(){console.log("hello world")}
+//console.log(function(){});
+const url = 'https://yogesh%20gaikwad%20portfolio';
+console.log(url.replace(/%20/g,"_"));
+

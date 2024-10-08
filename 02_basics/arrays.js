6e64e7317394e0d803b1655a887692c37b29dfde
yogesh <71713475+yo10-ai@users.noreply.github.com>
2024-10-08 04:12:03 +0000

    octsevensaves

---
+++ 02_basics/arrays.js
@@ -0,0 +1,6 @@
+const newarray = [2,3,4,5,6]
+const slicearray = newarray.slice(2);
+// console.log(slicearray);
+console.log(newarray);
+const splicearray = newarray.splice(0,3);
+console.log(splicearray);

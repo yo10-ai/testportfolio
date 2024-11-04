c0cef8333cc4ba6276076b5d87f0639f54ab828c
yogesh <71713475+yo10-ai@users.noreply.github.com>
2024-11-04 08:35:19 +0000

    destructuring of object

--- 02_basics/objectsone.js
+++ 02_basics/objectsone.js
@@ -21,7 +21,14 @@ const obj1={1:'a', 2:'b', 3:'c'}
 const obj2={3:'d', 5:'e', 6:'f'}
 
 const obj3=Object.assign({},obj1,obj2);
-console.log(obj3);
+// console.log(obj3);
 /*********another way to merge the two objects***/
 const obj={...obj1,...obj2};
-console.log(obj);
+// console.log(obj);
+
+/************************destructuring of objects************/
+const course={coursename:"learnjseasy", courseprice:9999, instructor:"hiteshbhaupatil"}
+
+const {coursename: CN, courseprice: cp, instructor:ins} = course;
+
+console.log(CN);
